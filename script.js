0d168e6356abda5db6ff693f9c798aa3de4620fd
Mike Davis <micdavis93@gmail.com>
2022-10-24 14:03:46 -0600

    Changed headers

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "Below is a list of Mike's compositions:";
 
 document.querySelector("body").appendChild(h2);
